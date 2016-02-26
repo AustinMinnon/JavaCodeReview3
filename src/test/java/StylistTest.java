c284804ddd42b4ae1208c97fd2e58572191f95d0
austinminnon <whi7ee@gmail.com>
2016-02-26 13:46:11 -0800

    add passing test for getting all clients listed in stylist category

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -33,4 +33,16 @@ public class StylistTest {
     Stylist savedStylist = Stylist.find(myStylist.getId());
     assertTrue(myStylist.equals(savedStylist));
   }
+
+  @Test
+  public void getClients_RetrievesAllClientsFromDatabase_clients() {
+    Stylist myStylist = new Stylist("Robert");
+    myStylist.save();
+    Client firstClient = new Client("Karin", myStylist.getId());
+    firstClient.save();
+    Client secondClient = new Client("Janelle", myStylist.getId());
+    secondClient.save();
+    Client[] clients = new Client[] { firstClient, secondClient };
+    assertTrue(myStylist.getClients().containsAll(Arrays.asList(clients)));
+  }
 }
