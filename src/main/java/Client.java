f8d4881c47ccc17c1ccd3ad674ca4e21df3a956a
austinminnon <whi7ee@gmail.com>
2016-02-26 13:25:19 -0800

    add passing test for equals override

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,5 +6,34 @@ public class Client {
   private int stylistId;
   private String name;
 
-  
+  public int getId() {
+      return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getStylistId() {
+    return stylistId;
+  }
+
+  public Client(String name, int stylistId) {
+    this.name = name;
+    this.stylistId = stylistId;
+  }
+
+  @Override
+  public boolean equals(Object otherClient){
+    if (!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      System.out.println(this.getStylistId());
+      System.out.println(newClient.getStylistId());
+      return this.getName().equals(newClient.getName()) &&
+             this.getId() == newClient.getId() &&
+             this.getStylistId() == newClient.getStylistId();
+    }
+  }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -11,5 +11,10 @@ public class ClientTest {
     assertEquals(Client.all().size(), 0);
   }
 
-
+  @Test
+  public void equals_returnTrueIfClientsAreEqual() {
+    Client firstClient = new Client("Jerry", 1);
+    Client secondClient = new Client("Jerry",1);
+    assertTrue(firstClient.equals(secondClient));
+  }
 }
